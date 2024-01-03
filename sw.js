d3cdd6551492a5cff5f0815fd595cacb318ec9f1
pocas <pocas@jojeong-won-ui-MacBookPro.local>
2024-01-03 21:06:07 +0900

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704282131';
+const cacheName = 'chirpy-1704283507';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
