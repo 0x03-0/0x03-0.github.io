6e5b74a23922912b27c9fc1a0035c9824cc272fe
pocas <pocas@pocasui-MacBookPro.local>
2025-04-23 02:25:56 +0900

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1745342463';
+const cacheName = 'chirpy-1745342729';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
